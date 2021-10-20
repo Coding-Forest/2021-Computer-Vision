78c7e73a23b124b9e49bc7a0e8a6d5a92f7aa029
Coding Forest <64945652+Coding-Forest@users.noreply.github.com>
2021-10-20 15:00:29 +1100

    Create ImageDifference.py

---
+++ 00 Fundamentals/Image Difference/ImageDifference.py
@@ -0,0 +1,12 @@
+# References: Jie Jenn (2019) How to compare two images and display the differences using Python (Hint: Pillow) https://www.youtube.com/watch?v=fUfvBnREBFc&ab_channel=JieJenn
+
+from PIL import Image, ImageChops
+
+img1 = Image.open("difference/vlcsnap-00005.png")
+img2 = Image.open("difference/vlcsnap-00006.png")
+
+diff = ImageChops.difference(img1, img2)
+
+if (diff.getbbox()):
+    diff.show()                          # shows the image.
+    diff.save("difference/diff1.png")    # save the difference imge object.
